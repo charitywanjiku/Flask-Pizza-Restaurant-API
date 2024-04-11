9558b29f0e1f870c9ec1f65588be375b0222f67d
charitywanjiku <charity.wanjiku1@student.moringaschool.com>
2024-04-12 00:18:10 +0300

    done

--- server/model.py
+++ server/model.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import validates
 
 db = SQLAlchemy()
 
@@ -7,6 +8,13 @@ class Restaurant(db.Model):
     name = db.Column(db.String(50), unique=True, nullable=False)
     address = db.Column(db.String(100), nullable=False)
     pizzas = db.relationship('Pizza', secondary='restaurant_pizza', backref='restaurants')
+    @validates('name')
+    def validate_name(self, key, name):
+        if len(name.strip()) == 0:
+            raise ValueError('Name cannot be empty')
+        if len(name) > 50:
+            raise ValueError('Name must be less than 50 characters')
+        return name
 
 class Pizza(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -18,3 +26,13 @@ class RestaurantPizza(db.Model):
     price = db.Column(db.Float, nullable=False)
     restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'), nullable=False)
     pizza_id = db.Column(db.Integer, db.ForeignKey('pizza.id'), nullable=False)
+    @validates('price')
+    def validate_price(self, key, price):
+        if price < 1 or price > 30:
+            raise ValueError('Price must be between 1 and 30')
+        return price
+
+
+  
+
+    
